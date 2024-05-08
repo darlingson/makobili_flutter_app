ebe15ba9ec3744e9a998174d179ac4ac86bfb5d9
darlingson <dmakuwila@gmail.com>
2024-05-08 10:25:21 +0200

    added the custom card to the homescreen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:makobili/components/balance_card.dart';
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Home Page',
-          style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-    );
+    return Column(children: [
+      SizedBox(
+        height: 200,
+        child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: 10,
+          itemBuilder: (context, index) {
+            return BalanceCard(index: index);
+          },
+        ),
+      )
+    ]);
   }
 }
