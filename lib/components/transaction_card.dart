fcd94a67c6342fd3f3afcfb29fecfdf493561789
darlingson <dmakuwila@gmail.com>
2024-05-08 22:15:33 +0200

    added a transactions card

---
+++ lib/components/transaction_card.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class TransActionCard extends StatelessWidget {
+  final String accountNumber;
+  final String accountType;
+  final String institution;
+  final String amount;
+  final String transactionDirection;
+  const TransActionCard(
+      {super.key,
+      required this.accountNumber,
+      required this.institution,
+      required this.accountType,
+      required this.amount,
+      required this.transactionDirection});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+        width: 320,
+        child: Card(
+            color: Colors.blue[100], // Light blue card color for the theme
+            child: Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Account Number: $accountNumber',
+                        style: TextStyle(fontSize: 16)),
+                    Text('Institution: $institution',
+                        style: TextStyle(fontSize: 16)),
+                    Text('Account Type: $accountType',
+                        style: TextStyle(fontSize: 16))
+                  ]),
+            )));
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,28 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:makobili/components/balance_card.dart';
+import 'package:makobili/components/transaction_card.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  const HomeScreen({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      SizedBox(
-        height: 250,
-        child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          itemCount: 10,
-          itemBuilder: (context, index) {
-            return BalanceCard(
-              index: index,
-              accountName: 'Account ${index + 1}',
-              accountNumber: '1234 5678 9012 345${index + 1}',
-              institution: 'Bank ${index + 1}',
-              accountType: 'Savings',
-            );
-          },
+    return Column(
+      children: [
+        SizedBox(
+          height: 250,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return BalanceCard(
+                index: index,
+                accountName: 'Account ${index + 1}',
+                accountNumber: '1234 5678 9012 345${index + 1}',
+                institution: 'Bank ${index + 1}',
+                accountType: 'Savings',
+              );
+            },
+          ),
         ),
-      )
-    ]);
+        Expanded(
+          child: ListView.builder(
+            itemBuilder: (context, index) {
+              return TransActionCard(
+                accountNumber: '1234 5678 9012 345${index + 1}',
+                institution: 'Bank ${index + 1}',
+                accountType: 'Savings',
+                amount: 'Account ${index + 1}',
+                transactionDirection: "out",
+              );
+            },
+            itemCount: 10,
+          ),
+        ),
+      ],
+    );
   }
 }
