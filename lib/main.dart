0704305177233ca0ec31f69e722f8be7e8e5b741
darlingson <dmakuwila@gmail.com>
2024-05-08 09:51:49 +0200

    created the bottomNavBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Finance Tracker',
       theme: ThemeData(
-        // Light theme settings
         brightness: Brightness.light,
-        primaryColor: Color(0xFF0D47A1),
+        primaryColor: const Color(0xFF0D47A1),
         fontFamily: 'Roboto',
         textTheme: const TextTheme(
           displayLarge: TextStyle(
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
               fontSize: 14.0, fontFamily: 'Hind', color: Colors.black87),
         ),
         buttonTheme: ButtonThemeData(
-          buttonColor: Color(0xFF1976D2),
+          buttonColor: const Color(0xFF1976D2),
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(8.0),
           ),
@@ -33,52 +34,90 @@ class MyApp extends StatelessWidget {
             ColorScheme.fromSwatch().copyWith(secondary: Color(0xFF64FFDA)),
       ),
       darkTheme: ThemeData(
-        // Define a color scheme for the dark theme with the appropriate brightness
-        colorScheme: ColorScheme.dark(
+        colorScheme: const ColorScheme.dark(
           primary: Color(0xFF0D47A1),
           secondary: Color(0xFF64FFDA),
-          onPrimary:
-              Colors.white, // Ensuring text/icons on primary color are white
-          onSecondary:
-              Colors.black, // Ensuring text/icons on secondary color are black
+          onPrimary: Colors.white,
+          onSecondary: Colors.black,
         ),
         fontFamily: 'Roboto',
-        buttonTheme: ButtonThemeData(
+        buttonTheme: const ButtonThemeData(
           buttonColor: Color(0xFF1976D2),
           textTheme: ButtonTextTheme.primary,
         ),
-        textTheme: TextTheme(
-          headline1: TextStyle(
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
               fontSize: 72.0, fontWeight: FontWeight.bold, color: Colors.white),
-          headline6: TextStyle(
+          titleLarge: TextStyle(
               fontSize: 36.0, fontStyle: FontStyle.italic, color: Colors.white),
-          bodyText2: TextStyle(
+          bodyMedium: TextStyle(
               fontSize: 14.0, fontFamily: 'Hind', color: Colors.white70),
         ),
       ),
-      themeMode: ThemeMode
-          .system, // Decides which theme to show based on the system preference
+      themeMode: ThemeMode.system,
       home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+
+  // List of widgets to show in the body based on selected tab
+  static List<Widget> _widgetOptions = <Widget>[
+    Text('Home Page',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    Text('Accounts Page',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    Text('Reports Page',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+    Text('Settings Page',
+        style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Finance Overview'),
+        title: Text('Finance Tracker'),
       ),
       body: Center(
-        child: Text('Welcome to Your Finance Tracker!'),
+        child: _widgetOptions.elementAt(_selectedIndex),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Add Transaction',
-        child: Icon(Icons.add),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_balance_wallet),
+            label: 'Accounts',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart),
+            label: 'Reports',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.blue[800],
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
       ),
     );
   }
