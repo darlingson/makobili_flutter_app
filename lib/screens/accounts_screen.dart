52c3240cc02662d0442c2eed17f46355c02bf430
darlingson <dmakuwila@gmail.com>
2024-05-30 00:17:55 +0200

    added code to retrieve accounts from local db

--- lib/screens/accounts_screen.dart
+++ lib/screens/accounts_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:makobili/components/add_account_form.dart';
-import 'package:makobili/components/balance_card.dart';
 import 'package:makobili/models/account.dart';
+import 'package:makobili/utils/database_helper.dart';
 
 class AccountsScreen extends StatefulWidget {
   const AccountsScreen({Key? key});
@@ -11,7 +11,22 @@ class AccountsScreen extends StatefulWidget {
 }
 
 class _AccountsScreenState extends State<AccountsScreen> {
-  List<Account> accounts = [];
+  List<Account> _accounts = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchAccounts();
+  }
+
+  Future<void> _fetchAccounts() async {
+    final accounts = await DatabaseHelper.instance.fetchAccounts();
+    print('accounts: $accounts');
+    setState(() {
+      _accounts = accounts;
+    });
+  }
+
   void _showAddAccountDialog(BuildContext context) {
     showDialog(
       context: context,
@@ -42,15 +57,65 @@ class _AccountsScreenState extends State<AccountsScreen> {
       ),
       body: ListView.builder(
         itemBuilder: (context, index) {
-          return BalanceCard(
+          return AccountCard(
             index: index,
-            accountName: 'Account ${index + 1}',
-            accountNumber: '1234 5678 9012 345${index + 1}',
-            institution: 'Bank ${index + 1}',
-            accountType: 'Savings',
+            accountName: _accounts[index].name,
+            accountNumber: _accounts[index].accountNumber,
+            institution: _accounts[index].institution,
+            accountType: _accounts[index].type,
           );
         },
-        itemCount: 10,
+        itemCount: _accounts.length,
+      ),
+    );
+  }
+}
+
+class AccountCard extends StatelessWidget {
+  final int index;
+  final String accountName;
+  final String accountNumber;
+  final String institution;
+  final String accountType;
+
+  const AccountCard({
+    Key? key,
+    required this.index,
+    required this.accountName,
+    required this.accountNumber,
+    required this.institution,
+    required this.accountType,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 350,
+      child: Card(
+        color: Colors.blue[100],
+        elevation: 5,
+        margin: const EdgeInsets.all(8),
+        shape: RoundedRectangleBorder(
+          borderRadius:
+              BorderRadius.circular(4.0), // Small radius for minimal rounding
+          side: const BorderSide(color: Colors.black87, width: 1.0), // Border
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text('Account Name: $accountName',
+                  style: const TextStyle(fontSize: 16)),
+              Text('Account Number: $accountNumber',
+                  style: const TextStyle(fontSize: 16)),
+              Text('Institution: $institution',
+                  style: const TextStyle(fontSize: 16)),
+              Text('Account Type: $accountType',
+                  style: const TextStyle(fontSize: 16)),
+            ],
+          ),
+        ),
       ),
     );
   }
