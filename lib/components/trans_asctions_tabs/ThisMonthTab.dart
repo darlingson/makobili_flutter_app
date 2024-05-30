aee4a6922fe42e69691e062d97abea6a6f7705bf
darlingson <dmakuwila@gmail.com>
2024-05-30 20:23:03 +0200

    view transactions in this month tab

--- lib/components/trans_asctions_tabs/ThisMonthTab.dart
+++ lib/components/trans_asctions_tabs/ThisMonthTab.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:makobili/models/transaction.dart';
 import 'package:makobili/utils/database_helper.dart';
@@ -16,6 +14,19 @@ class _ThisMonthTabState extends State<ThisMonthTab> {
 
   Future<void> _fetchTransactions() async {
     final transactions = await DatabaseHelper.instance.fetchTransactions();
+    //iterate through the list of transactions and only keep those that are from this month
+    List<BankTransaction> filteredTransactions = [];
+    for (var transaction in transactions) {
+      if (transaction.date.month == DateTime.now().month) {
+        filteredTransactions.add(transaction);
+      }
+    }
+
+    if (filteredTransactions.isNotEmpty) {
+      _transactions = filteredTransactions;
+    } else {
+      _transactions = [];
+    }
     setState(() {
       _transactions = transactions;
     });
@@ -29,11 +40,26 @@ class _ThisMonthTabState extends State<ThisMonthTab> {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'This Month Page',
-        style: TextStyle(fontSize: 20),
-      ),
-    );
+    return Expanded(
+        child: ListView.builder(
+            itemCount: _transactions.length,
+            itemBuilder: (context, index) {
+              return Card(
+                elevation: 5,
+                margin: const EdgeInsets.all(10),
+                color: _transactions[index].direction == 'in'
+                    ? Colors.green
+                    : Colors.red,
+                child: ListTile(
+                  visualDensity: VisualDensity(
+                    horizontal: 0,
+                    vertical: -4,
+                  ),
+                  leading: Text(_transactions[index].direction),
+                  title: Text(_transactions[index].description),
+                  subtitle: Text(_transactions[index].amount.toString()),
+                ),
+              );
+            }));
   }
 }
