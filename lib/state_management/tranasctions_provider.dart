3a977e78bbf59e1c3cbfca091b4478dfdeccc281
darlingson <dmakuwila@gmail.com>
2024-07-09 18:53:55 +0200

    modified the transactions provider

--- lib/state_management/tranasctions_provider.dart
+++ lib/state_management/tranasctions_provider.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:makobili/models/transaction.dart';
+import 'package:makobili/database_helper.dart';
 
 class TransActionsProvider extends ChangeNotifier {
-  late List<BankTransaction> transactions = [];
+  List<BankTransaction> transactions = [];
 
-  void updateTransactions(List<BankTransaction> transactions) {
-    this.transactions = transactions;
+  Future<void> fetchTransactions(int accountId) async {
+    transactions = await DatabaseHelper.instance.fetchTransactionsByAccountId(accountId);
     notifyListeners();
   }
 
